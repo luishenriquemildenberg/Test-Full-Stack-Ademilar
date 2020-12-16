cfda8c7b2d531ccd9811ca5a442faa6cd5771f1f
Luis Henrique <hard.henrique@Outlook.com]>
2020-12-16 08:51:39 -0300

    modificação cliente, insert

--- insert.php
+++ insert.php
@@ -4,28 +4,29 @@
 
     $dbClass = new DataBase(); //instanciando a classe
 
-if (isset($_POST['email']) && empty(!$_POST['email'])) { 
+	if (isset($_POST['email']) && empty(!$_POST['email'])) { 
 
-    // echo'<pre>';
-    // print_r($_POST);
-    // die();
+		// echo'<pre>';
+		// print_r($_POST);
+		// die();
 
-    $nome  = $_POST['nome'];
-    $email = $_POST['email'];
-    $senha = $_POST['pass'];
+		$nome  = $_POST['nome'];
+		$email = $_POST['email'];
+		$senha = $_POST['pass'];
 
-    try {
+		try {
 
-        $sql = "INSERT INTO cliente 
-                (nome, email, senha) 
-                VALUES 
-                ('{$nome}', '{$email}', '{$senha}')
-            ";
-            
-           $q = $dbClass->execute($sql);
-           return $q;       
-        header("Location: index.html");
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-    }
-}
+			$sql = "INSERT INTO cliente 
+					(nome, email, senha) 
+					VALUES 
+					('{$nome}', '{$email}', '{$senha}')
+				";
+				
+			$q = $dbClass->execute($sql);
+			   
+			header("Location: cliente.html");
+			
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
